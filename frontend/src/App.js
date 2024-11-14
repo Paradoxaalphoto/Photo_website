968a0894e95ef1e2c9868b8f9e023fc6c3acd70e
Paradoxaalphoto <paradoxaalphoto@gmail.com>
2024-11-13 22:06:09 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import Gallery from './Gallery';
 
+function App() {
+  return (
+    <div>
+      <h1>Photo Gallery</h1>
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
