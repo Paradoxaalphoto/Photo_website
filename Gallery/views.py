5a713d32a57a9257842db7e6fad11eaf97f8698c
Paradoxaalphoto <paradoxaalphoto@gmail.com>
2024-11-13 21:30:30 -0500

    Update views.py

--- Gallery/views.py
+++ Gallery/views.py
@@ -1,1 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Photo
+from .serializers import PhotoSerializer
 
+class PhotoListView(APIView):
+    def get(self, request):
+        photos = Photo.objects.all()
+        serializer = PhotoSerializer(photos, many=True)
+        return Response(serializer.data)
