b2ffeeb8d4d6e7d741b9a29316098bd5d2c8687f
Paradoxaalphoto <paradoxaalphoto@gmail.com>
2024-11-14 02:32:14 -0500

    Update models.py

--- Gallery/models.py
+++ Gallery/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Photo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='gallery/')  # Make sure upload_to is set to a valid directory
+    image = models.ImageField(upload_to='media/')  # Make sure upload_to is set to a valid directory
