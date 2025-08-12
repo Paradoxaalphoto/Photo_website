7d69f8eb097a2c7c3efc298aca95d9d400e8a36e
Paradoxaalphoto <paradoxaalphoto@gmail.com>
2025-08-12 05:16:02 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// If using Firebase in Alpha, paste your config below and import this file in your code.
+// Example:
+// export const firebaseConfig = {
+//   apiKey: "YOUR_API_KEY",
+//   authDomain: "YOUR_PROJECT.firebaseapp.com",
+//   projectId: "YOUR_PROJECT",
+//   storageBucket: "YOUR_PROJECT.appspot.com",
+//   messagingSenderId: "XXXX",
+//   appId: "1:XXXX:web:XXXX"
+// };
