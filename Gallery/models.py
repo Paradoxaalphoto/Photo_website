3b84d798c1779a07486bc2418e78f29fb6a693ad
Paradoxaalphoto <paradoxaalphoto@gmail.com>
2025-08-12 05:23:08 -0400

    Delete Gallery directory

--- Gallery/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Photo(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='media/')  # Make sure upload_to is set to a valid directory
--- Gallery/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Photo
-
-class PhotoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Photo
-        fields = ('id', 'title', 'description', 'image', 'uploaded_at')
