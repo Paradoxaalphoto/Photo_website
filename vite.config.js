b99e77eea06e66c7c618e739277dfb5a47f2a5ac
Paradoxaalphoto <paradoxaalphoto@gmail.com>
2025-08-21 22:33:35 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace base with your repo name when deploying to GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/yorn/', // <-- change to '/<your-repo-name>/'
+  base: '/photo_website/', // <-- change to '/<your-repo-name>/'
 })
