68011dcfda245f9f684d270ce6f47a259c78e95c
Paradoxaalphoto <paradoxaalphoto@gmail.com>
2025-09-06 00:13:24 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Replace base with your repo name when deploying to GitHub Pages
-export default defineConfig({
-  plugins: [react()],
-  base: '/photo_website/', // <-- change to '/<your-repo-name>/'
-})
